b61326fb88ef3f5cb161dc8a626f52b02f10ac76
rickfellas <rykelmecahu01@gmail.com>
2026-05-30 17:25:34 -0500

    feat: atualização das rotas - validação de token

--- src/routes/gameRoutes.ts
+++ src/routes/gameRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { getGuidesForGame, getAllGames } from "../controllers/gameController";
+import { authMiddleware } from "../middlewares/authMiddleware"; // <-- 1. importação add
 
 const router = Router();
 
-router.get("/", getAllGames);
+// 2. authMiddleware no meio da rota principal
+router.get("/", authMiddleware, getAllGames);
 router.get("/:gameId/guides", getGuidesForGame);
 
 export default router;
